44e5aba32557074c496071683501c6af11757028
Morten Wang <morten@cs.umn.edu>
2015-06-25 12:57:13 -0500

    Switch to default cursor to allow with-usage

--- suggestbot/db.py
+++ suggestbot/db.py
@@ -54,7 +54,7 @@ class SuggestBotDatabase:
         try:
             self.conn = MySQLdb.connect(read_default_file=self.default_file,
                                         charset='utf8')
-            self.cursor = self.conn.cursor(MySQLdb.cursors.SSDictCursor)
+            self.cursor = self.conn.cursor()
             return True
         except MySQLdb.Error as e:
             logging.error("Unable to connect to database.")
