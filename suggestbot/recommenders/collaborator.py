0878d09794ec5adc816bca97021bf5cb83a43276
stuem007 <stuem007@mail.cs.umn.edu>
2015-06-26 14:40:16 -0500

    Add missing import
    Failed to import operator, operator fail

--- suggestbot/recommenders/collaborator.py
+++ suggestbot/recommenders/collaborator.py
@@ -21,7 +21,9 @@ Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
 Boston, MA  02110-1301, USA.
 '''
 
+import operator
 import logging
+
 import MySQLdb
 
 from suggestbot import config
