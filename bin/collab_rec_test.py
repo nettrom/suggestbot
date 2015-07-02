77347e0c2803ee5e5df0d450cd84777971c97126
Morten Wang <morten@cs.umn.edu>
2015-07-02 10:49:51 -0500

    Updated test size and printout of statistics

--- bin/collab_rec_test.py
+++ bin/collab_rec_test.py
@@ -22,6 +22,7 @@ Boston, MA  02110-1301, USA.
 '''
 
 import re
+import random
 import logging
 
 from suggestbot.recommenders.collaborator import CollabRecommender
@@ -42,6 +43,9 @@ def main():
         cli_parser.add_argument('member_file', type=str,
                                 help='path to member file')
 
+        # cli_parser.add_argument('k', type=int,
+        #                     help='size of random sample to draw')
+
         cli_parser.add_argument('nrecs', type=int,
                                 help='number of recommendations per user')
         
@@ -59,14 +63,41 @@ def main():
                 for line in infile:
                         match_obj = member_re.search(line.strip())
                         all_members.add(match_obj.group('username'))
+
+        # members = random.sample(all_members, args.k)
+        recommender = CollabRecommender()
+        site = pywikibot.Site('en') 
                
         print("Beginning collaborator recommendation test")
-        recommender = CollabRecommender()
 
-        members = ['Kieran4', 'Brendandh', 'Gog the Mild', 'Seitzd', 'Robotam',
-                   'Keith-264', 'Nyth83', 'Mmuroya', 'Navy2004', 'Secutor7',
-                   'Ranger Steve', 'MisterBee1966']
-        site = pywikibot.Site('en') 
+        total_recs = 0
+        total_overlap = 0
+
+        members = ['Slatersteven', 'WerWil', 'Fnlayson', 'Drrcs15', 'Turbothy',
+                   '21stCenturyGreenstuff', 'RGFI', 'Loesorion', 'Grahamdubya', 'Sioraf',
+                   'Skittles the hog', 'Smoth 007', 'Superfly94', 'Ewulp', 'Dank', 'Magus732',
+                   'Redmarkviolinist', 'The27thMaine', 'Kcdlp', 'Foxsch', 'Tdrss', 'URTh',
+                   'Waase', 'L clausewitz', 'Judgedtwice', 'Choy4311', 'Codrinb', 'Smmurphy',
+                   'Kliu1', 'Gowallabies', 'Secutor7', 'Moneywagon', 'Nostalgia of Iran',
+                   'Linmhall', 'Karanacs', 'Dana boomer', 'Robotam', 'Fdsdh1', 'DanieB52',
+                   'Rosiestep', 'Scholarus', 'Laurinavicius', 'Dapi89', 'UrbanTerrorist',
+                   'AGK', 'Samuel Peoples', 'Sapphire', 'Catlemur', 'Martocticvs', 'Gparkes',
+                   'Pratyya Ghosh', 'Eurocopter', 'Pahari Sahib', 'Seitzd', 'The Bushranger',
+                   'Natobxl', 'MasterOfHisOwnDomain', 'Takashi kurita', 'TeunSpaans',
+                   'Kierzek', 'WDGraham', 'Miborovsky', 'The lost library',
+                   'Antidiskriminator', 'The ed17', 'Cliftonian', 'AshLin',
+                   'GeneralizationsAreBad', 'MechaChrist', 'Joep01', 'Chris.w.braun',
+                   'TBrandley', 'Marky48', 'Cplakidas', 'John', 'Nyth83', 'Elonka',
+                   'Alexandru.demian', 'Martinp23', 'GermanJoe', 'P.Marlow', 'ryan.opel',
+                   'Asarelah', 'Ian Rose', 'Pectory', 'KizzyB', 'MrDolomite', 'Leifern',
+                   'Timeweaver', 'Ashashyou', 'Sumsum2010', 'Looper5920', 'Geira', 'Ackpriss',
+                   'Binksternet', 'Lothar von Richthofen', 'Molestash', 'Srnec',
+                   'Sasuke Sarutobi', '.marc.']
+
+        # members = ['Kieran4', 'Brendandh', 'Gog the Mild', 'Seitzd', 'Robotam',
+        #            'Keith-264', 'Nyth83', 'Mmuroya', 'Navy2004', 'Secutor7',
+        #            'Ranger Steve', 'MisterBee1966']
+
 
         for member in members:
                 user = pywikibot.User(site, member)
@@ -81,10 +112,18 @@ def main():
                 match_set = set([rec['item'] for rec in matches])
                 overlap = match_set & all_members
 
+                total_recs += len(match_set)
+                total_overlap += len(overlap)
+
                 print('Got {n} recommendations for User:{user}'.format(n=len(match_set),
                                                                        user=member))
                 print('Overlap with all members: {0}'.format(len(overlap)))
 
+        print('''Total statistics:
+    Number of recommendations: {n}
+    Overlap with all members: {o}
+    % overlap: {p:.2}'''.format(n=total_recs, o=total_overlap,
+                                p=100*float(total_overlap)/float(total_recs)))
         print('Recommendation test complete')
 	
 if __name__ == "__main__":
