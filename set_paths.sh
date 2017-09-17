0372a15bbb419000d6229b52bcff9bb0023c8798
Morten Wang <nettrom@gmail.com>
2017-09-17 16:51:09 -0700

    Update paths for the new VM
    Pywikibot's configuration and the Python 3 virtual environment moved into new locations.

--- set_paths.sh
+++ set_paths.sh
@@ -13,7 +13,7 @@
 # having an account with them all to ourselves.
 
 scriptdir=`pwd`;
-export PYWIKIBOT2_DIR='/export/scratch/morten/suggestbot/pywikibot';
+export PYWIKIBOT2_DIR=$HOME/.pywikibot;
 # export PYWIKIPEDIA_DIR='/export/scratch/morten/work/SuggestBot/pywikipedia';
 export SUGGESTBOT_DIR=$scriptdir;
 # export PYTHONPATH=$SUGGESTBOT_DIR
@@ -21,4 +21,4 @@ export NLTK_DATA=$scriptdir/nltk_data
 
 # Load SuggestBot's own virtual environment
 # source /export/scratch/morten/sbotenv/bin/activate
-export PYTHON_EXECUTABLE="/export/scratch/morten/sb_py36_venv/bin/python3"
+export PYTHON_EXECUTABLE=$HOME/venv/suggestbot/bin/python;
