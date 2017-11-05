7fee574846791f703a320ec8ed68fd340a30695d
Morten Wang <nettrom@gmail.com>
2017-11-05 08:24:17 -0800

    Move inlink count tables to tools database
    The old database servers on Toolforge (c1 and c3) are being decomissioned.
    New servers are online, but do not allow creation of databases, meaning our
    inlink table needed to move to the Tools database server. This commit
    defines the tables on the Tools DB server, and updates the script that
    populates those tables so data is grabbed from the new analytics-
    oriented replicated Wikipedia database servers.

--- tool-labs/link-rec/inlink-count-table.sql
+++ tool-labs/link-rec/inlink-count-table.sql
@@ -1,25 +1,38 @@
-USE p50380g50553__ilc;
+-- USE p50380g50553__ilc;
+USE s51172__ilc_p;
 
 -- Format of the inlink count table used by links-server.py
 CREATE TABLE enwiki_inlinkcounts (
        ilc_page_id INT(8) UNSIGNED NOT NULL PRIMARY KEY, -- page ID
        ilc_numlinks INTEGER UNSIGNED DEFAULT 0, -- # of inlinks
        ilc_age TINYINT UNSIGNED DEFAULT 0 -- num days since last update
-) ENGINE=InnoDB
+) ENGINE=InnoDB;
+
+-- Create tables for all the other Wikipedias:
+CREATE TABLE fawiki_inlinkcounts LIKE enwiki_inlinkcounts;
+CREATE TABLE frwiki_inlinkcounts LIKE enwiki_inlinkcounts;
+CREATE TABLE nowiki_inlinkcounts LIKE enwiki_inlinkcounts;
+CREATE TABLE ptwiki_inlinkcounts LIKE enwiki_inlinkcounts;
+CREATE TABLE ruwiki_inlinkcounts LIKE enwiki_inlinkcounts;
+CREATE TABLE svwiki_inlinkcounts LIKE enwiki_inlinkcounts;
 
 CREATE TABLE inlinkcount_updates (
        ilcu_lang VARCHAR(16) NOT NULL PRIMARY KEY, -- language code
        ilcu_timestamp DATETIME, -- timestamp of last edited page read
        ilcu_update_running TINYINT DEFAULT 0 -- set if this wiki is currently being updated
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Insert default values for English Wikipedia where that is hosted
+-- Insert default values for all Wikipedias
 INSERT INTO inlinkcount_updates
 (ilcu_lang, ilcu_timestamp)
-VALUES ('en', NULL)
+VALUES ('en', NULL),
+       ('fa', NULL),
+       ('fr', NULL),
+       ('no', NULL),
+       ('pt', NULL),
+       ('ru', NULL),
+       ('sv', NULL);
 
--- Insert default values for Norwegian, Swedish, and Portuguese
--- where those are hosted.
-INSERT INTO inlinkcount_updates
-(ilcu_lang, ilcu_timestamp)
-VALUES ('no', NULL), ('sv', NULL), ('pt', NULL);
+
+
+
--- tool-labs/link-rec/update_linkcounts.sh
+++ tool-labs/link-rec/update_linkcounts.sh
@@ -12,8 +12,11 @@
 # Store errors in a different place.
 #$ -e $HOME/logs/inlink-update.err
 
-# Ask for 512MB of memory
-#$ -l h_vmem=512M
+# Ask for 1GB of memory
+#$ -l h_vmem=1024M
+
+# Run on trusty to have all necessary libraries
+#$ -l release=trusty
 
 # Command-line option is the language we're updating for
-python $HOME/link-rec/inlink-table-updater.py -l $1
+$HOME/venv/3.4/bin/python3 $HOME/link-rec/inlink-table-updater.py $1
